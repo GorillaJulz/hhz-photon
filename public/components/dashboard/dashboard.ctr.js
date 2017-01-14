26d420dc61ab50670d4757d4df8a2325fe183b9a
Julian Skupnjak <skupnjak@de.ibm.com>
2017-01-14 14:20:31 +0100

    make webui work

--- public/components/dashboard/dashboard.ctr.js
+++ public/components/dashboard/dashboard.ctr.js
@@ -16,8 +16,8 @@
 
         var connect = function() {
 
-          ws = new WebSocket("ws://hhz-photon.eu-gb.mybluemix.net/ws");
-          //ws = new WebSocket("ws://"+ window.location.host + "/ws");
+
+          ws = new WebSocket("ws://"+ window.location.host + "/ws");
 
           ws.onopen = function (e) {
             console.log("onopen:", arguments);
@@ -37,13 +37,13 @@
             console.log("onerror:", arguments);
           };
         };
-        connect();
+
 
 
       		//initial value of dataPoints
       		var dps = [
-      		{button: "like", count: 5, time: 'green'},
-          {button: "dislike", count: 5, time: 'red'}
+      		{button: "like", count: 5, color: 'green'},
+          {button: "dislike", count: 5, color: 'red'}
       		];
 
       		var chart = new CanvasJS.Chart("chartContainer",{
@@ -79,40 +79,23 @@
 
                   break;
                 case 'dislike':
-                  dps[1] = {label: dps[1].label , y: dps[1].y, color: 'red'};
+                  dps[1] = {label: data.button , y: data.count, color: 'red'};
                   dctr.dlike = data.count;
                   break;
                 default:
 
               }
-
-
-
-      			// for (var i = 0; i <1; i++) {
-            //
-      				// // generating random variation deltaY
-      				// var deltaY = Math.round(2 + Math.random() *(-2-2));
-      				// var yVal = deltaY + dps[i].y > 0 ? dps[i].y + deltaY : 0;
-      				// var boilerColor;
-              //
-              //
-      				// // color of dataPoint dependent upon y value.
-              //
-      				//  boilerColor =
-      				// yVal > 200 ? "#FF2500":
-      				//   dps[i].label == 'like' ? "#FF6000":
-      				//   dps[i].label == 'dislike' ? "#6B8E23 ":
-      				//  null;
-              //
-
-      				// updating the dataPoint
-      			// 	dps[i] = {label: dps[i].label , y: dps[i].y, color: dps[i].boilerColor};
-            //
-      			// };
-
       			chart.render();
       		};
 
-      		//updateChart();
+          function initChart() {
+            for (var i = 0; i <1; i++) {
+            	dps[i] = {label: dps[i].button , y: dps[i].count, color: dps[i].color};
+            }
+            chart.render();
+          }
+
+      		initChart(dps);
+          connect();
        }
 })();
